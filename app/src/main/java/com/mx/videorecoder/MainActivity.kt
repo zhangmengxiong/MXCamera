5ceca528b85b45d8a4b999934b8f107bd9db6cd9
zmx_f <zmx_final@163.com>
2020-03-04 12:31:17 +0800

    v1.0.2 修复倒计时显示的BUG

--- app/src/main/java/com/mx/videorecoder/MainActivity.kt
+++ app/src/main/java/com/mx/videorecoder/MainActivity.kt
@@ -19,32 +19,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO),
-                1
+            this,
+            arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO),
+            1
         )
 
         zpBtn.setOnClickListener {
             val config = CameraConfigBuild.createSimplePicConfig(this)
             val intent = Intent(this, RecorderActivity::class.java)
-                    .putExtra(RecorderActivity.CONFIG, config)
+                .putExtra(RecorderActivity.CONFIG, config)
             startActivityForResult(intent, 0x22)
         }
         spBtn.setOnClickListener {
-            val config = CameraConfigBuild.createSimple3GPConfig(this)
+            val config = CameraConfigBuild.createSimple3GPConfig(this).apply {
+                maxDuration = 10
+            }
             val intent = Intent(this, RecorderActivity::class.java)
-                    .putExtra(RecorderActivity.CONFIG, config)
+                .putExtra(RecorderActivity.CONFIG, config)
             startActivityForResult(intent, 0x22)
         }
 
         zp2Btn.setOnClickListener {
             val build = CameraConfigBuild(CameraConfig.TYPE_PIC)
-                    .setJpegQuality(30)
-                    .setExpectSize(720, 1280)
-                    .setOutputFile(File(cacheDir, "aaa.jpg").absolutePath)
+                .setJpegQuality(30)
+                .setExpectSize(720, 1280)
+                .setOutputFile(File(cacheDir, "aaa.jpg").absolutePath)
             val config = build.build()
             val intent = Intent(this, RecorderActivity::class.java)
-                    .putExtra(RecorderActivity.CONFIG, config)
+                .putExtra(RecorderActivity.CONFIG, config)
             startActivityForResult(intent, 0x22)
         }
     }
@@ -55,9 +57,9 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 0x22) {
             val file = (data?.getSerializableExtra(RecorderActivity.RESULT_KEY) as File?) ?: return
             Toast.makeText(
-                    this,
-                    "${file.name} ${file.length() / 1024f} Kb",
-                    Toast.LENGTH_SHORT
+                this,
+                "${file.name} ${file.length() / 1024f} Kb",
+                Toast.LENGTH_SHORT
             ).show()
         }
     }
